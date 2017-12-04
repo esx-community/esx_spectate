b115a07132ca3682930c93964612f20077d01419
Cem <cemayder97@gmail.com>
2017-12-04 23:57:00 +0100

    better styling

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,32 @@
 ﻿$(function(){
 
-	$('#main').on('click', '.player', function(){
+	$('.users').on('click', '.spec', function(){
 
-		let target = $(this).attr('id');
-		let player = $('#main').attr('id');
+		let target = $(this).data('spectate');
+
+		let player = $('.spectate').attr('id');
+
+
 		if (target === player) {
 			alert("You can't spectate yourself !");
 		} else {
-			$('#main').fadeOut();
-			$.post('http://esx_spectate/select', JSON.stringify({target}));
+			$('.spectate').fadeOut();
+			$.post('http://esx_spectate/select', JSON.stringify({id: target}));
 		}
 
 	});
 
-	$('#main').on('click', '.btnKick', function(){
+	$('.users').on('click', '.kick', function(){
 
-		let target = $(this).attr('id');
+		let target = $(this).data('kick');
 
 		let reason = prompt("KICK REASON", "Pseudo RP SVP");
 
 		if (reason == null || reason == "") {
 		    alert("MISSING KICK REASON")
 		} else {
-		    $('#main').fadeOut();
-			$.post('http://esx_spectate/kick', JSON.stringify({reason}));
+		    $('.spectate').fadeOut();
+			$.post('http://esx_spectate/kick', JSON.stringify({id: target, reason: reason}));
 		}
 	});
 
@@ -31,17 +34,17 @@
 		if (event.data.type == "show"){
 			let data = event.data.data;
 			let player = event.data.player;
-			$('#main').attr('id', player);
+			$('.spectate').attr('id', player);
 			populate(data);
 			setTimeout(function(){
-				$('#main').fadeIn();
+				$('.spectate').fadeIn();
 			}, 200)
 		}
 	});
 
 	$(document).keyup(function(e){
 		if (e.keyCode == 27){
-			$('#main').fadeOut();
+			$('.spectate').fadeOut();
 			$.post('http://esx_spectate/close');
 		}
 	})
@@ -49,7 +52,7 @@
 });
 
 function populate(data){
-	$('#main #players').html('');
+	$('.spectate .users').html('');
 
 	data.sort(function(a, b) {
 		let idA = a.id;
@@ -65,13 +68,16 @@ function populate(data){
 		let id = data[i].id;
 		let name = data[i].name;
 
-		let element = 	'<tr class="player" id="' + id + '">' +
-							'<td class="player_id">' + id + '</td>' +
-							'<td class="player_name">' + name + '</td>' +
-							'<td class="btn_kick" id="' + id + '">KICK</td>' +
-						'</tr>';
+		let element = 	'<div class="user">' +
+							'<span class="user-id">' + id + '</span>' +
+							'<span class="user-name">' + name + '</span>' +
+							'<span class="user-actions">' +
+								'<button class="spec" data-spectate="' + id + '">Spectate</button>' +
+								'<button class="kick" data-kick="' + id + '">KICK</button>' +
+							'</span>' +
+						'</div>';
 
-		$('#players').append(element);
+		$('.spectate .users').append(element);
 	}
 
 }
